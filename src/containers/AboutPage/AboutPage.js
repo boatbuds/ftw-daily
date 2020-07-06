7e5104ec72942f21cd138befe323d0cd481139c6
Shailaja <shailu@boatbuds.com>
2020-07-05 20:30:27 -0600

    updated commit page

--- src/containers/AboutPage/AboutPage.js
+++ src/containers/AboutPage/AboutPage.js
@@ -40,49 +40,44 @@ const AboutPage = () => {
 
           <div className={css.contentWrapper}>
             <div className={css.contentSide}>
-              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
+              <p>Boats don't run on thanks!</p>
             </div>
 
             <div className={css.contentMain}>
-              <h2>
-                Most of the Finnish saunas are located at the homes of individuals - indeed, most
-                people in Finland live in an apartment with sauna in it. In addition, lots of people
-                have lakeside summer cottages, which also typically come with a separate sauna
-                building near the waterfront.
-              </h2>
+        
 
               <p>
-                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
-                instead visit a real home or cottage sauna. Saunatime makes this possible for
-                everyone. All our saunas are owned by individuals willing to let tourists and other
-                curious visitors to enter their sacred spaces.
+              My girlfriend and I had made plans one warm summer day with a buddy who owned a boat. 
+              We spent the night before getting everything ready to go and our excitement was building. 
+              That morning we woke up early, swung by the corner coffee stand to get us through the hour drive, and headed down the road. 
+              Just as we were pulling up to the lake, our friends cancelled on us. 
+              </p>
+              <p>
+              There we were, ready to have some fun in the sun but stranded without options. 
+              Stuck at the dock, we somberly watched the boats pull in and smiles float away. 
+              We decided to salvage what we could of the day and hike around to a popular point. 
+              After sun bathing for a few hours, my girlfriend received a text from her friend saying they were out on their boat and would swing by and grab us. 
+              As we were climbing on, they explained they had been there all day without any company and that they wish they had known we were there earlier.
+              Knowing boats don’t run on ‘thank yous’, I apologized I didn’t have any cash or brews to compensate for their good deed. 
+              They cruised us around for about an hour and dropped us back off, we instantly felt better. 
+              As we sat back on the beach watching the boats float by with a new perspective, it hit me; why aren’t we connected on the water?
               </p>
 
-              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
-
               <p>
-                Saunatime offers you a good way to earn some extra cash! If you're not using your
-                sauna every evening, why not rent it to other people while it's free. And even if
-                you are using your sauna every evening (we understand, it's so good), why not invite
-                other people to join you when the sauna is already warm! A shared sauna experience
-                is often a more fulfilling one.
-              </p>
-
-              <h3 id="contact" className={css.subtitle}>
-                Create your own marketplace like Saunatime
-              </h3>
-              <p>
-                Saunatime is brought to you by the good folks at{' '}
-                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
-                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
-                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
-                get in touch!
+              The idea of Boat Buds was born out of a need on the lake that day. 
+              It would be a peer to peer network that would join the people who owned boats to people who wanted to go out on them. 
+              This wouldn’t be a rental scheme, rather a way for people to broaden their network and a platform that would facilitate a fair exchange in 
+              order for both parties to have a good time. Not only does Boat Buds link friends who are going out together on a boat, we make 
+              it possible to make plans on any body of water by connecting strangers with the same goal—to have some fun in the sun. 
+              It would be a tool that I wish I had many other times that same summer. 
+              The more people the idea spread to, the more positive momentum grew and that idea on the lake that day slowly turned into a community.
               </p>
               <p>
                 You can also checkout our{' '}
-                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
-                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
+                <ExternalLink href="https://www.facebook.com/theboatbuds">Facebook</ExternalLink> and{' '}
+                <ExternalLink href="https://www.instagram.com/theboatbuds">Twitter</ExternalLink>.
               </p>
+              <strong>Capt. Jake</strong>
             </div>
           </div>
         </LayoutWrapperMain>
